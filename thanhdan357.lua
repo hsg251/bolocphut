6b660b2fe41490246947051849e163e88f00142b
hsg251 <lethanhson25102011@gmail.com>
2024-12-20 21:29:46 +0700

    thanhdan357.lua

--- thanhdan357.lua
+++ thanhdan357.lua
@@ -30,4 +30,4 @@ wait(2)
 TextLabel:Destroy()
 
 -- Thực thi script từ URL
-loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruit/refs/heads/main/Source.lua"))()
+getgenv().Team = "Marines" getgenv().AutoLoad = false getgenv().SlowLoadUi = false getgenv().ForceUseSilentAimDashModifier = false getgenv().ForceUseWalkSpeedModifier = false loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
