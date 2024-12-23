dcd32ec467484dc4f6a29a1dd8f182482c39e0e0
hsg251 <lethanhson25102011@gmail.com>
2024-12-23 20:54:04 +0700

    thanhdan357.lua

--- thanhdan357.lua
+++ thanhdan357.lua
@@ -13,23 +13,23 @@ TextLabel.BackgroundTransparency = 1
 TextLabel.Position = UDim2.new(0.5, -150, 0.05, 0)  -- Đưa thông báo lên gần trên cùng
 TextLabel.Size = UDim2.new(0, 300, 0, 50)
 TextLabel.Font = Enum.Font.SourceSansBold
-TextLabel.Text = "Khi bạn nhập script này bạn sẽ trở nên đẹp trai đấy"
+TextLabel.Text = "có một tin bùn=("
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 30
 TextLabel.TextStrokeTransparency = 0.5
 TextLabel.TextScaled = true
 
 -- Đợi 5 giây trước khi thay đổi thông báo
-wait(5)
+wait(2)
 
 -- Thay đổi thông báo thành "Đã tải xong"
-TextLabel.Text = "lúc trước chỉ lấy trên mạng để mn xài đở thôi giờ update xog rồi"
+TextLabel.Text = "mệt quá không làm bên bloxfruits nx=("
 
 --tiếp
-TextLabel.Text = "THANKS FOR USING!!=))"
+TextLabel.Text = "vì tao lười nên lấy đại cái trên mạng"
 
 -- Hiển thị thông báo "Đã tải xong" trong 2 giây rồi xóa
-wait(2)
+wait(3)
 TextLabel:Destroy()
 
 -- Bắt đầu script
@@ -37,9 +37,9 @@ repeat task.wait() until game:IsLoaded() -- Chờ đến khi game được tải
 local TablePlace = {7449423635, 2753915549, 4442272183} -- Danh sách các PlaceId hợp lệ
 game:GetService("StarterGui"):SetCore("SendNotification", { -- Gửi thông báo cho người chơi
     Title = "thông báo", -- Tiêu đề thông báo
-    Text = "Bây giờ tên Hub đã được đổi=)", -- Nội dung thông báo
-    Icon = "rbxassetid://9709149431", -- Biểu tượng thông báo (tuỳ chọn)
-    Duration = 10 -- Thời gian hiển thị thông báo (10 giây)
+    Text = "cái này là trên mạng", -- Nội dung thông báo
+    Icon = "rbxassetid://113954644931303", -- Biểu tượng thông báo (tuỳ chọn)
+    Duration = 5 -- Thời gian hiển thị thông báo (10 giây)
 })
 
 if table.find(TablePlace, game.PlaceId) then -- Kiểm tra xem game.PlaceId có nằm trong danh sách hợp lệ không
