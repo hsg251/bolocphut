25a780f844f231861a7273574e10fa543bcb516e
hsg251 <lethanhson25102011@gmail.com>
2025-01-26 13:58:25 +0700

    mau.lua

--- mau.lua
+++ mau.lua
@@ -1,49 +1,15 @@
-local player = game.Players.LocalPlayer
-local screenGui = Instance.new("ScreenGui")
-screenGui.Parent = player:WaitForChild("PlayerGui")
-
-local label = Instance.new("TextLabel")
-label.Size = UDim2.new(0, 150, 0, 30)  -- Kích thước nhỏ hơn
-label.Position = UDim2.new(1, -160, 0, 10)  -- Đặt ở góc trên bên phải
-label.BackgroundTransparency = 1
-label.TextColor3 = Color3.fromRGB(255, 255, 255)  -- Màu chữ trắng
-label.TextSize = 14  -- Chữ nhỏ
-label.Text = "Thời gian AFK: 00:00"
-label.TextXAlignment = Enum.TextXAlignment.Right  -- Căn phải
-label.Parent = screenGui
-
--- Lưu thời gian bắt đầu
-local startTime = tick()
-
--- Cập nhật thời gian
-local function updateTime()
-    while true do
-        local elapsedTime = tick() - startTime
-        local minutes = math.floor(elapsedTime / 60)
-        local seconds = math.floor(elapsedTime % 60)
-        label.Text = string.format("Thời gian AFK: %02d:%02d", minutes, seconds)
-        
-        wait(1)  -- Cập nhật mỗi giây
+game.Players.PlayerAdded:Connect(function(player)
+    if player.Name == "Mie", "quangdatvip19", "thanh_dan357", "thanh_nhan567" then
+        player:Kick("tài khoản của bạn đã bị cấm khỏi sever script ThanhDan Hub")
     end
-end
-
--- Bắt đầu cập nhật thời gian
-spawn(updateTime)
-
--- Đoạn mã Loadstring gốc của bạn, thực thi ngay lập tức khi script được chạy
-spawn(function()
-    local url = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"
-    local scriptContent = game:HttpGet(url)
-    loadstring(scriptContent)()  -- Chạy mã từ URL
 end)
 
--- Xử lý thực thi cho từng PlaceID
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        spawn(function()
-            local url = Execute
-            local scriptContent = game:HttpGet(url)
-            loadstring(scriptContent)()  -- Chạy mã cho game cụ thể
-        end)
-    end
-end
+wait(1)
+print("người chơi đã ko bị cấm!")
+print("vui lòng chờ!")
+
+wait(3)
+getgenv().Team = "Pirates"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+
+
